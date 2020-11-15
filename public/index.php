9898dd87cdbf1360843431e8c5a3e3c4d0446456
mysteriou <elio007@hotmail.fr>
2020-11-15 20:36:49 +0100

    ajout variable php mode dans l'index

--- public/index.php
+++ public/index.php
@@ -9,22 +9,20 @@ require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+$mode = null;
 
-    Debug::enable();
+if($_SERVER['HTTP_HOST'] == "localhost"){
+
+$mode = "dev";
+
+}else{
+
+$mode = "prod";
+
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
-
-  $kernel = new Kernel('dev', true);
+$kernel = new Kernel($mode, true);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
